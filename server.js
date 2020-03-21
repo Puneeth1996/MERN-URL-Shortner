69002256bcaca9cbb2376cadfa9cb24b673dc77d
Puneeth1996 <puneeth1996p@gmail.com>
2020-03-21 16:04:09 +0530

    will keep making the progress

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded(( extended: false )));
+
+const db = require('./config/key').mongoURI
+
+mongoose.connect(db)
+    .then(onfullfilled:() => console.log('MongoDB connected.'))
+    .catch(onrejected: err => console.log(err));
+
 app.get('/', (req, res) => {
-    res.send("Hello World");
-})
+    res.send("Hello World this is with nodemon . . .");
+});
 
 const port =  process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
