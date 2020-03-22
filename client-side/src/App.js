2426aec384df7612fbfb55843855f9c3f82db707
Puneeth1996 <puneeth1996p@gmail.com>
2020-03-22 16:07:11 +0530

    working code both in front and server side

---
+++ client-side/src/App.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react'
+// import logo from './logo.svg';
+// import './App.css';
+
+import { Navbar,Nav,NavDropdown,Form,Row,Col,Jumbotron,Container,Button } from 'react-bootstrap';
+
+
+
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { URL: '', shortURL: '', };
+  }
+  myChangeHandler = (event) => {
+    this.setState({URL: event.target.value});
+  }
+
+  mySubmitHandler = (event) => {
+    event.preventDefault();
+    console.log("You are submitting " + this.state.URL);
+    // send a POST request
+    axios.post('http://localhost:5000/api/url/shorten', {
+      "longUrl": this.state.URL
+    })
+      .then((response) => {
+        console.log(response);
+      }, (error) => {
+        console.log(error);
+      });
+    }
+
+
+  render() {
+    return (
+      <>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand href="#home">Insta - Car</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">More deets</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              Dank memes
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      
+      
+      
+      <Jumbotron fluid>
+        <Container>
+        <Form onSubmit={this.mySubmitHandler}>
+          <Form.Group as={Row} controlId="formHorizontalEmail">
+            <Form.Label column sm={2}>
+              Enter URL
+            </Form.Label>
+            <Col sm={8}>
+              <Form.Control type="url" placeholder="Please Enter The Url To Be Shortened" onChange={this.myChangeHandler} />
+            </Col>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form.Group>
+        </Form>
+          <p>
+            This is a modified jumbotron that occupies the entire horizontal space of
+            its parent.
+          </p>
+        </Container>
+      </Jumbotron>
+      
+      </>
+    )
+  }
+}
