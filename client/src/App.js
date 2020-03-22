de4166f67cf2652a12b4b9c0db109a2c9e59291b
Puneeth1996 <puneeth1996p@gmail.com>
2020-03-22 12:34:21 +0530

    Making the UI Now time for implementing the functionality

--- client/src/App.js
+++ client/src/App.js
@@ -2,31 +2,58 @@ import React from 'react';
 // import logo from './logo.svg';
 // import './App.css';
 
-import { Navbar,Nav,NavDropdown,Form,FormControl,Button } from 'react-bootstrap';
+import { Navbar,Nav,NavDropdown,Form,Row,Col,Jumbotron,Container } from 'react-bootstrap';
 
 function App() {
   return (
-    <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-        <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-success">Search</Button>
-        </Form>
-      </Navbar.Collapse>
-    </Navbar>
+<>
+
+<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Navbar.Brand href="#home">Insta - Car</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+      <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+    <Nav>
+      <Nav.Link href="#deets">More deets</Nav.Link>
+      <Nav.Link eventKey={2} href="#memes">
+        Dank memes
+      </Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
+
+
+
+  <Jumbotron fluid>
+    <Container>
+    <Form>
+      <Form.Group as={Row} controlId="formHorizontalEmail">
+        <Form.Label column sm={2}>
+          Enter URL
+        </Form.Label>
+        <Col sm={10}>
+          <Form.Control type="url" placeholder="Please Enter The Url To Be Shortened" />
+        </Col>
+      </Form.Group>
+    </Form>
+      <p>
+        This is a modified jumbotron that occupies the entire horizontal space of
+        its parent.
+      </p>
+    </Container>
+  </Jumbotron>
+
+</>
   );
 }
 
