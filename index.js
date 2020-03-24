50f3c5da71a700490993718460805151a71f5530
Puneeth1996 <puneeth1996p@gmail.com>
2020-03-24 09:01:33 +0530

    preparing heroku deployment

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const connectDB = require('./config/db');
 
+const path = require("path")
+
 const app = express();
 
 // Connect to database
@@ -14,16 +16,17 @@ app.use('/api/url', require('./routes/url'));
 
 
 
-const path = require("path")
 const PORT = process.env.NODE_ENV || 5000;
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "client", "build")))
+    app.use(express.static('client/build'))
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+
 }
 
 
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
