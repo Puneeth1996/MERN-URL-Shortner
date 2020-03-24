0402c9fec8ba19b4a4e74744575fda56a8c6b9dc
Puneeth1996 <puneeth1996p@gmail.com>
2020-03-24 08:46:48 +0530

    making the deployment settings for the heroku

--- index.js
+++ index.js
@@ -12,9 +12,18 @@ app.use(express.json());
 app.use('/', require('./routes/index'));
 app.use('/api/url', require('./routes/url'));
 
+
+
+const path = require("path")
 const PORT = process.env.NODE_ENV || 5000;
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, './client/build')));
+    app.use(express.static(path.join(__dirname, "client", "build")))
 }
+
+
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
