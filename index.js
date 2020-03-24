69da725a27cfd0284f5fc5953bcf610616ea6078
Puneeth1996 <puneeth1996p@gmail.com>
2020-03-24 09:46:44 +0530

    making the push, trying to run on heroku

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const connectDB = require('./config/db');
 
+const path = require("path")
+
+
 const app = express();
 
 // Connect to database
@@ -12,6 +15,18 @@ app.use(express.json());
 app.use('/', require('./routes/index'));
 app.use('/api/url', require('./routes/url'));
 
-const PORT = 5000;
+
+
+
+const PORT = process.env.NODE_ENV || 5000;
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static('client/build'))
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+
+}
+
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
