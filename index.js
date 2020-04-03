8c5c3cdc7f202abdaed6cd9ec50379cf88c75aba
Puneeth1996 <puneeth1996p@gmail.com>
2020-04-03 17:42:46 +0530

    clearning the fuss

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use('/api/url', require('./routes/url'));
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static( 'client/build' ));
 
-    app.get('*', (req, res) => {
+    app.get('/', (req, res) => {
         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); // relative path
     });
 }
